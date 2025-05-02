666a6458d5ea579bad71586cf06afe4fd19c85e3
Angie <rodriguezangela95@gmail.com>
2025-05-02 19:27:30 +0200

    feat: validations

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,14 +1,31 @@
+import path from "node:path";
 import express from "express";
 import createError from "http-errors";
 import logger from "morgan";
+import * as homeController from "./controllers/homeController.js"
 
 const app = express();
 
-app.use(logger("dev"));
+app.set("views", "views");
+app.set("view engine", "html");
+app.engine("html", (await import("ejs")).__express);
 
-app.get("/", (req, res, next) => {
-    res.send("hello");
-});
+
+app.locals.appName = "NodeApp"
+
+app.use(logger("dev"));
+/* app.use(express.static("public")); */
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(import.meta.dirname, "public")));
+
+/**
+ * Application routes
+ */
+app.get("/", homeController.index);
+app.get("/param-in-route/:num", homeController.paramInRoute);
+app.get("/param-in-route-multiple/:product/size/:size/color/:color", homeController.paramInRouteMultiple);
+app.get("/param-in-query", homeController.validateParamInQuery, homeController.paramInQuery);
+app.post("/post-with-body", homeController.postWhithBody);
 
 app.use((req, res, next) => {
     //const error = new Error("Algo ha salido mal...");
@@ -18,7 +35,19 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
-    res.send(`Error ${err.status} ${err.message}`);
+    //res.send(`Error ${err.status} ${err.message}`);
+    if (err.array) {
+        err.message = "Invalid request: " + err.array().map(e =>
+            `${e.location} ${e.type} "${e.path}" ${e.msg}`
+        ).join(" ");
+        err.status = 422;
+        console.log(err);
+    }
+    
+    res.locals.message = err.message
+    res.locals.error = process.env.NODEAPP_ENV === "development" ? err : {}
+    
+    res.render("error");
 });
 
 export default app;
---
+++ nodeapp/controllers/homeController.js
@@ -0,0 +1,56 @@
+import { query, validationResult } from "express-validator";
+
+export function index (req, res, next) {
+    //res.locals.appName = "NodeApp"
+    
+    res.locals.users = [
+        {name: "Smith", age: 45},
+        {name: "Brown", age: 28},
+        {name: "Jones", age: 34}
+    ];
+    
+    const now = new Date();
+    res.locals.isEven = (now.getSeconds() % 2) === 0;
+    res.locals.thisSecond = now.getSeconds();
+    
+    res.locals.code = "<script>alert(Injected!)</script>";
+    
+    res.render("home", /*{appName: "NodeApp"}*/);
+};
+
+export function paramInRoute (req, res, next) {
+    const num = req.params.num;
+    res.send("me has pasado " + num);
+};
+
+export function paramInRouteMultiple (req, res, next) {
+    const product = req.params.product;
+    const size = req.params.size;
+    const color = req.params.color;
+
+    res.send(`Quieres un ${product} de talla ${size} color ${color}`);
+};
+
+export function paramInQuery (req, res, next) {
+    validationResult(req).throw();
+
+    const color = req.query.color;
+
+    console.log(req.query);
+
+    res.send(`El color es ${color}`);
+};
+
+export function postWhithBody (req, res, next) {
+    const {age, color} = req.body;
+    res.send("OK!");
+};
+
+export const validateParamInQuery = [
+    query("color").notEmpty().custom(value => {
+        ["red","blue"].includes(value);
+        return value === "blue"
+        }
+    ).withMessage("must be red or blue"),
+    query("size").isNumeric().withMessage("must be a number")
+];
